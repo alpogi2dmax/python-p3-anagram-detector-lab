ec88a252c615ae85b1b1e62fc64f17625e243471
Al Cerdan Lico <alcerdan@yahoo.com>
2024-11-22 19:01:57 -0800

    complete python anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_array):
+        match_array = []
+        for x in word_array:
+            if sorted(self.word) == sorted(x):
+                match_array.append(x)
+        return match_array
